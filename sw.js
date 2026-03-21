b7083918575690baea623f60330f4bc296ab6b81
freepdfconverterr <mohmd.armaan135@gmail.com>
2026-03-21 20:27:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Freepdfconverterr Service Worker
 // Version 4.0
 
-const CACHE_NAME = 'freepdfconverterr-v6';
+const CACHE_NAME = 'freepdfconverterr-v7';
 const ASSETS = [
   '/',
   '/index.html',
